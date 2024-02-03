b5b742bf649a28955f4268221a4175806cbb2eb0
Jose-Fernandes-de-Souza-Junior <99496702+Jose-Fernandes-de-Souza-Junior@users.noreply.github.com>
2024-02-02 21:37:30 -0400

    Add files via upload

---
+++ Codigo Orientado a Objetos/CotacaoDolar/ConversorDeMoeda.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CotacaoDolar
+{
+    internal class ConversorDeMoeda
+    {
+        public static double dolar;
+        public static double real;
+
+        static public double Converter(){
+            double valor = real * dolar;
+            
+            return valor+(valor*0.06);
+            
+        }
+    }
+}
---
+++ Codigo Orientado a Objetos/CotacaoDolar/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Faça um programa para ler a cotação do dolar, e depois um valor em dolares para ser comprado por uma pessoa em reais.
+ Informar quantos reais a pessoa vai pagar pelos dolares, considerando ainda que a pessoa devera pagar 6% de IOF sobre o valor do dolar.
+Criar uma classe ConversorDeMoeda para ser responsavel pelos calculos.*/
+
+using System.Globalization;
+
+namespace CotacaoDolar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Qual é a cotação do dolar? R$ ");
+            ConversorDeMoeda.real = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.Write("Quantos Dolares vai comprar? $ ");
+            ConversorDeMoeda.dolar = double.Parse(Console.ReadLine(),CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Valor a ser pago em reais = R$ "+ ConversorDeMoeda.Converter().ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
+
