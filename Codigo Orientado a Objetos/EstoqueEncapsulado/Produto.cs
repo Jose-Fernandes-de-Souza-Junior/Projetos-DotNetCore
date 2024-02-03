a3bec5256be7632aacf5e7658c3f7f06467215f9
Jose-Fernandes-de-Souza-Junior <99496702+Jose-Fernandes-de-Souza-Junior@users.noreply.github.com>
2024-02-02 21:39:19 -0400

    Add files via upload

---
+++ Codigo Orientado a Objetos/EstoqueEncapsulado/Produto.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EstoqueEncapsulado
+{
+    internal class Produto
+    {
+        private string _nome;
+        public double Preco { get; private set; } //Auto Properties para proibir o Preço e a Quantidade de serem alterados em outros arquivos
+        public int Quantidade { get; private set; }
+
+        public Produto(string nome, double preco, int quantidade)
+        {
+            _nome = nome;
+            Preco = preco;
+            Quantidade = quantidade;
+        }
+
+        public string Nome {                //Propertie para controlar o acesso ao atributo nome em outros arquivos
+            get { return _nome;} 
+
+            set { if (value != null && value.Length > 1)
+                {
+                    _nome = value;
+                }
+            }
+        } 
+         
+
+        public double ValorTotalEmEstoque()
+        {
+            return Preco * Quantidade;
+        }
+
+        public void AdicionarProduto(int quantidade)
+        {
+            Quantidade += quantidade;
+        }
+
+        public void RemoverProduto(int quantidade)
+        {
+            Quantidade -= quantidade;
+        }
+
+        public override string ToString()
+        {
+            return _nome + ", R$ " + Preco.ToString("F2", CultureInfo.InvariantCulture) + ", "
+                + Quantidade + " unidades, Total: R$ " + ValorTotalEmEstoque().ToString("F2", CultureInfo.InvariantCulture);
+        }
+
+
+    }
+}
+
+
+
---
+++ Codigo Orientado a Objetos/EstoqueEncapsulado/Program.cs
@@ -0,0 +1,45 @@
+﻿using EstoqueEncapsulado;
+using System;
+using System.Globalization;
+
+namespace Course
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Produto p = new Produto("TV",500.0,10);
+
+            /*Console.WriteLine("Entre com os dados do Produto.");
+            Console.Write("Nome: ");
+            p.SetNome(Console.ReadLine()) ;*/
+            p.Nome = "T";
+            
+
+            Console.WriteLine("Dados do produto: " + p.Nome);
+            Console.WriteLine("Preço do produto: " + p.Preco);
+            /*Console.Write("Preço: ");
+            p.Preco = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.Write("Quantidade no estoque: ");
+            p.Quantidade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados do produto: " + p.GetNome());
+
+            Console.Write("Digite a quantidade de produtos a ser adicionada em estoque: ");
+            int qtde = int.Parse(Console.ReadLine());
+
+            p.AdicionarProduto(qtde);
+
+            Console.WriteLine("Dados atualizados: " + p);
+
+            Console.Write("Digite a quantidade de produtos a ser removida em estoque: ");
+            qtde = int.Parse(Console.ReadLine());
+
+            p.RemoverProduto(qtde);
+
+            Console.WriteLine("Dados atualizados: " + p);*/
+
+        }
+    }
+}
